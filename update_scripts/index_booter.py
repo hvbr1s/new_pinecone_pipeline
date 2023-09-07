d5e3e94a18b6381e76727897415f79ac81502c7c
Hvbris <44342309+hvbr1s@users.noreply.github.com>
2023-09-07 14:40:13 +0200

    Update index_booter.py

--- update_scripts/index_booter.py
+++ update_scripts/index_booter.py
@@ -7,7 +7,7 @@ load_dotenv()
 PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 PINECONE_ENVIRONMENT = os.getenv('PINECONE_ENVIRONMENT')
 
-def reboot_index(index_name: str = 'academyzd'):
+def reboot_index(index_name: str = 'prod'):
     # initialize connection to pinecone
     pinecone.init(
         api_key=PINECONE_API_KEY,
